4516c1038c10ace01541775ba082903050a6e680
Noxx <dev@noxx.digital>
2022-11-12 05:23:03 +0100

    HttpStream.php added. (similar to psr7)

---
+++ src/HttpStream.php
@@ -0,0 +1,263 @@
+<?php
+
+namespace Neon\Http;
+
+use RuntimeException;
+use Trinity\http\ArgumentException;
+
+class HttpStream
+{
+    protected $stream;
+
+    /**
+     * @throws ArgumentException
+     */
+    public function __construct(
+        private readonly string $mode
+    )
+    {
+        if(
+            $this->mode === 'r' ||
+            $this->mode === 'r+' ||
+            $this->mode === 'w' ||
+            $this->mode === 'w+' ||
+            $this->mode === 'a' ||
+            $this->mode === 'a+' ||
+            $this->mode === 'x' ||
+            $this->mode === 'x+' ||
+            $this->mode === 'c' ||
+            $this->mode === 'c+'
+        )
+        {
+            $this->stream = fopen( 'php://temp', $this->mode );
+        }
+        else
+        {
+            throw new ArgumentException('Not existing mode Provided.');
+        }
+    }
+
+    /**
+     * Reads all data from the stream into a string, from the beginning to end.
+     *
+     * This method MUST attempt to seek to the beginning of the stream before
+     * reading data and read the stream until the end is reached.
+     *
+     * Warning: This could attempt to load a large amount of data into memory.
+     *
+     * This method MUST NOT raise an exception in order to conform with PHP's
+     * string casting operations.
+     *
+     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $this->rewind();
+        return $this->read( $this->get_size() );
+    }
+
+    /**
+     * Closes the stream and any underlying resources.
+     * Closes the stream and any underlying resources.
+     *
+     * @return void
+     */
+    public function close(): void
+    {
+       fclose( $this->stream );
+    }
+
+    /**
+     * Separates any underlying resources from the stream.
+     *
+     * After the stream has been detached, the stream is in an unusable state.
+     *
+     * @return resource|null Underlying PHP stream, if any
+     */
+    protected function detach()
+    {
+        return NULL;
+    }
+
+    /**
+     * Get the size of the stream if known.
+     *
+     * @return int|null Returns the size in bytes if known, or null if unknown.
+     */
+    public function get_size(): int|null
+    {
+        return fstat( $this->stream )['size'];
+    }
+
+    /**
+     * Returns the current position of the file read/write pointer
+     *
+     * @return int Position of the file pointer
+     * @throws \RuntimeException on error.
+     */
+    public function tell(): int
+    {
+
+       return ftell( $this->stream );
+    }
+
+    /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof(): bool
+    {
+        return feof( $this->stream );
+    }
+
+    /**
+     * Returns whether or not the stream is seekable.
+     *
+     * @return bool
+     */
+    public function is_seekable(): bool
+    {
+        return stream_get_meta_data( $this->stream )['seekable'];
+    }
+
+    /**
+     * Seek to a position in the stream.
+     *
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @param int $offset Stream offset
+     * @param int $whence Specifies how the cursor position will be calculated
+     *     based on the seek offset. Valid values are identical to the built-in
+     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
+     *     offset bytes SEEK_CUR: Set position to current location plus offset
+     *     SEEK_END: Set position to end-of-stream plus offset.
+     * @throws \RuntimeException on failure.
+     */
+    public function seek( int $offset, int $whence=SEEK_SET ): void
+    {
+        fseek( $this->stream, $offset, $whence );
+    }
+
+    /**
+     * Seek to the beginning of the stream.
+     *
+     * If the stream is not seekable, this method will raise an exception;
+     * otherwise, it will perform a seek(0).
+     *
+     * @see seek()
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @throws \RuntimeException on failure.
+     */
+    public function rewind(): void
+    {
+       rewind( $this->stream );
+    }
+
+    /**
+     * Returns whether or not the stream is writable.
+     *
+     * @return bool
+     */
+    public function is_writable(): bool
+    {
+        if(
+            $this->mode === 'r+' ||
+            $this->mode === 'w' ||
+            $this->mode === 'w+' ||
+            $this->mode === 'a' ||
+            $this->mode === 'a+' ||
+            $this->mode === 'x' ||
+            $this->mode === 'x+' ||
+            $this->mode === 'c' ||
+            $this->mode === 'c+'
+        )
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Write data to the stream.
+     *
+     * @param string $string The string that is to be written.
+     * @return int Returns the number of bytes written to the stream.
+     * @throws \RuntimeException on failure.
+     */
+    public function write( string $string ): int
+    {
+        if( $write = fwrite( $this->stream, $string, strlen( $string )) )
+            return $write;
+        else
+            throw new RuntimeException('Cannot write to stream.');
+    }
+
+    /**
+     * Returns whether or not the stream is readable.
+     *
+     * @return bool
+     */
+    public function is_readable(): bool
+    {
+        if(
+            $this->mode === 'r' ||
+            $this->mode === 'r+' ||
+            $this->mode === 'w+' ||
+            $this->mode === 'a+' ||
+            $this->mode === 'x+' ||
+            $this->mode === 'c+'
+        )
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Read data from the stream.
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *     them. Fewer than $length bytes may be returned if underlying stream
+     *     call returns fewer bytes.
+     * @return string Returns the data read from the stream, or an empty string
+     *     if no bytes are available.
+     * @throws \RuntimeException if an error occurs.
+     */
+    public function read( int $length ): string
+    {
+        if( $read = fread( $this->stream, $length ))
+            return $read;
+        else
+            throw new RuntimeException('Cannot read from stream.');
+    }
+
+    /**
+     * Returns the remaining contents in a string
+     *
+     * @return string
+     * @throws \RuntimeException if unable to read or an error occurs while
+     *     reading.
+     */
+    public function get_contents(): string
+    {
+        $this->read( $this->get_size() - $this->tell() );
+    }
+
+    public function get_metadata( $key=NULL )
+    {
+        $meta_data = stream_get_meta_data();
+        if( $key === NULL )
+            return $meta_data;
+        elseif( key_exists( $key, $meta_data ) )
+            return $meta_data[$key];
+        else
+            return NULL;
+    }
+}
